48858cbe1a5a978561ff497eb0d7811fa94f7ef6
Pablo Manfredi <manfredipablof@gmail.com>
2022-06-18 03:53:11 -0300

    Synchro

--- restartgrid.sh
+++ restartgrid.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 kubectl delete deployments --all;
 kubectl delete services --all;
+kubectl delete pods --all;
 sleep 30;
-kubectl apply -f full-grid-deployment.yaml
+kubectl apply -f full-grid-deployment.yaml;
+sleep 5;
+kubectl get pods -o wide
